e4b73b8117747f95850c3ce05bf5130f6f69037a
esogu <drgrzm@gmail.com>
2014-12-14 20:47:37 +0200

    deneme icin

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("S.A. Dunya ");
-        System.out.println("Edaaaaaa ");
+        System.out.println("\nEdaaaaaa ");
     }
 
 }
